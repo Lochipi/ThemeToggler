6e3538e7d2395b0f041b2ecec51de7650a78ecc1
Cornelius Emase <corneliuslochipi@gmail.com>
2023-10-06 01:19:58 +0300

    full setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dark Mode Togge
+
+Built an implementation of a toggler for dark and light mode.
+
+## Quick start
+
+Download the zip folder or clone it to your pc, then you can open your project in code editor of your choice, then follow this steps below.
+
+- `npm install`
+- `npm run dev`
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Main from "../components/Main";
+import { useState } from "react";
+
+export default function App() {
+  const [mode, setMode] = useState(false);
+
+  function toggle() {
+    setMode((prevState) => !prevState);
+  }
+
+  return (
+    <div className="container">
+      <Navbar darkMode={mode} toggleDarkMode={toggle} />
+      <Main darkMode={mode} />
+    </div>
+  );
+}
